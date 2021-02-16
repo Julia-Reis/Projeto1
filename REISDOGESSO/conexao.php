d5f421c5655db4f698a07dd6768611caaec56c80
Julia-Reis <60949284+Julia-Reis@users.noreply.github.com>
2021-02-16 11:52:32 -0300

    Update conexao.php

--- REISDOGESSO/conexao.php
+++ REISDOGESSO/conexao.php
@@ -1,5 +1,5 @@
 <?php
-    define('HOST', 'mysql380.umbler.com');
+    define('HOST', 'mysql380.umbler.com:41890');
     define('USUARIO', 'juliareis');
     define('SENHA', 'abracadabra');
     define('DB', 'reisdogesso');
