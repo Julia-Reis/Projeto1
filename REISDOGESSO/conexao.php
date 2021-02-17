5f266f494974739af7e5e381ceb8f9140ba53a9f
Julia-Reis <60949284+Julia-Reis@users.noreply.github.com>
2021-02-16 21:29:20 -0300

    Delete REISDOGESSO directory

--- REISDOGESSO/conexao.php
+++
@@ -1,8 +0,0 @@
-<?php
-    define('HOST', 'mysql380.umbler.com:41890');
-    define('USUARIO', 'juliareis');
-    define('SENHA', 'abracadabra');
-    define('DB', 'reisdogesso');
-
-    $conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possível conectar! Cheque as informações.');
-?>
